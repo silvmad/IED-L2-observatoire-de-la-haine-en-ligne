604a6d0957d789f7de4339316de3231efffd91a5
imenemes <imen.mes@gmail.com>
2021-04-08 23:16:36 +0200

    use config file

--- mydashapp.py
+++ mydashapp.py
@@ -23,10 +23,31 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
 
 app.server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# function for loading config setting
+def load_config(dictionnaire):
+     with open("config.sys", 'r') as flux:
+          load = flux.readlines()
+          for ligne in load:
+                conf = ligne.replace("\n", "")
+                conf = conf.split("\t")
+                dictionnaire[conf[0]] = conf[1]
+config = {}
+load_config(config)
+
+HOST = config.get("Bdd_host")
+USER = config.get("Bdd_login")
+PASSWORD = config.get("Bdd_secret")
+DATABASE = config.get("Bdd")
+
+
 # connecter à la bdd
-app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Rahma2011@localhost/haine_test1"
+try:
+    app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://%s:%s@%s/%s" % (USER,PASSWORD,HOST, DATABASE)
+except:
+    print("\tError during databse connexion")
 
 db = SQLAlchemy(app.server)
+
 # préparation de la dataframe en récupérant et manipulant le contenu de la BDD avec pandas
 df1 = pd.read_sql_table('corpus1', con=db.engine)
 df2 = pd.read_sql_table('possede', con=db.engine)
@@ -38,6 +59,7 @@ df4 = pd.merge(df2, df1[['id', 'date','haineux']], on="id")
 df7 = pd.merge(df4, df3, on="id_type")
 df8 = pd.merge(df7, df6, on="id")
 df = pd.merge(df8, df5, on="id_mot_clé")
+
 #convertir la colonne date écrite en string dans la bdd en date
 df['date'] = pd.to_datetime(df['date'])
 df.set_index('date', inplace=True)
@@ -59,10 +81,12 @@ app.layout = dbc.Container([
         dbc.Col(html.Div([
             dcc.DatePickerRange(
                 id='my-date-picker-range',
+                end_date_placeholder_text="date de fin",  # text that appears when no end date chosen
+                start_date_placeholder_text="date de début",  # text that appears when no start date chosen
+                first_day_of_week=1,  # Display of calendar when open (0 = Sunday)
                 min_date_allowed=dt(2017, 8, 5),
                 max_date_allowed=dt(2021, 5, 25),
                 initial_visible_month=dt(2021, 3, 1),
-                end_date=dt(2021, 12, 25),
             ),
         ]),
         ),
